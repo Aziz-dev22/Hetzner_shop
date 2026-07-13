e947fc7cbe6b977e9a96501e50729dfe1873b688
Aziz-dev22 <testnetmelli@gmail.com>
2026-07-13 07:49:02 +0330

    Add Base model for SQLAlchemy with Hetzner Shop

---
+++ database/base.py
@@ -0,0 +1,17 @@
+"""
+Hetzner Shop
+Database Base Model
+"""
+
+from __future__ import annotations
+
+
+from sqlalchemy.orm import DeclarativeBase
+
+
+
+class Base(
+    DeclarativeBase
+):
+
+    pass
