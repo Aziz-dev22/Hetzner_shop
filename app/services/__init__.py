aec03c135642346c60c631b9c970a729324b8246
Aziz-dev22 <testnetmelli@gmail.com>
2026-07-13 06:31:29 +0330

    Update __init__.py

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,15 +1,16 @@
-"""
-Hetzner Shop
-Service Registry
-"""
-
 from .user_service import (
     UserService,
+)
+
+from .product_service import (
+    ProductService,
 )
 
 
 __all__ = [
 
     "UserService",
+
+    "ProductService",
 
 ]
