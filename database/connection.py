b25cc4f1d2c338b4b7a624aeb7cce48b5c2bc0fc
Aziz-dev22 <testnetmelli@gmail.com>
2026-07-13 07:47:34 +0330

    Create connection.py

---
+++ database/connection.py
@@ -0,0 +1,71 @@
+"""
+Hetzner Shop
+Database Connection
+"""
+
+from __future__ import annotations
+
+
+from sqlalchemy.ext.asyncio import (
+
+    create_async_engine,
+
+    AsyncEngine,
+
+)
+
+
+from sqlalchemy.orm import (
+
+    sessionmaker,
+
+)
+
+
+from core.config import settings
+
+
+
+DATABASE_URL = (
+
+    f"postgresql+asyncpg://"
+
+    f"{settings.DATABASE_USER}:"
+
+    f"{settings.DATABASE_PASSWORD}@"
+
+    f"{settings.DATABASE_HOST}:"
+
+    f"{settings.DATABASE_PORT}/"
+
+    f"{settings.DATABASE_NAME}"
+
+)
+
+
+
+engine: AsyncEngine = create_async_engine(
+
+    DATABASE_URL,
+
+    echo=settings.APP_DEBUG,
+
+    pool_pre_ping=True,
+
+)
+
+
+
+AsyncSessionFactory = sessionmaker(
+
+    bind=engine,
+
+    expire_on_commit=False,
+
+)
+
+
+
+async def close_database():
+
+    await engine.dispose()
