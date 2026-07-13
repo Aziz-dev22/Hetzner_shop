cf6de1336c49273d7783dcdeebba4b0b125516c7
Aziz-dev22 <testnetmelli@gmail.com>
2026-07-13 05:10:20 +0330

    Refactor UnitOfWork to include repository instances

--- app/database/unit_of_work.py
+++ app/database/unit_of_work.py
@@ -7,29 +7,33 @@ from __future__ import annotations
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.repositories.user_repository import UserRepository
+from app.repositories.wallet_repository import WalletRepository
+from app.repositories.order_repository import OrderRepository
+from app.repositories.payment_repository import PaymentRepository
+
 
 class UnitOfWork:
 
     def __init__(self, session: AsyncSession):
         self.session = session
 
+        self.users = UserRepository(session)
+        self.wallets = WalletRepository(session)
+        self.orders = OrderRepository(session)
+        self.payments = PaymentRepository(session)
+
     async def __aenter__(self):
         return self
 
     async def __aexit__(self, exc_type, exc, tb):
-        if exc:
-            await self.rollback()
+        if exc_type:
+            await self.session.rollback()
         else:
-            await self.commit()
+            await self.session.commit()
 
     async def commit(self):
         await self.session.commit()
 
     async def rollback(self):
         await self.session.rollback()
-
-    async def flush(self):
-        await self.session.flush()
-
-    async def refresh(self, instance):
-        await self.session.refresh(instance)
