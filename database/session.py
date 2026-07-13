aa57c38722271de64e205502a143ebf809cdf28a
Aziz-dev22 <testnetmelli@gmail.com>
2026-07-13 07:48:20 +0330

    Create session.py

---
+++ database/session.py
@@ -0,0 +1,37 @@
+"""
+Hetzner Shop
+Database Session Manager
+"""
+
+from __future__ import annotations
+
+
+from typing import AsyncGenerator
+
+
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+
+from database.connection import (
+    AsyncSessionFactory,
+)
+
+
+
+async def get_session()
+-> AsyncGenerator[AsyncSession, None]:
+
+
+    async with AsyncSessionFactory() as session:
+
+
+        try:
+
+            yield session
+
+
+        finally:
+
+            await session.close()
