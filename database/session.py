516347af930d05157d51c9a8152c33f19004d82a
Aziz-dev22 <testnetmelli@gmail.com>
2026-07-13 10:39:04 +0330

    Refactor get_session to simplify session yield
    Simplified session management by removing try-finally block.

--- database/session.py
+++ database/session.py
@@ -5,7 +5,7 @@ Database Session Manager
 
 from __future__ import annotations
 
-from typing import AsyncGenerator
+from collections.abc import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -14,7 +14,4 @@ from database.connection import AsyncSessionFactory
 
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionFactory() as session:
-        try:
-            yield session
-        finally:
-            await session.close()
+        yield session
