0c8e4c2cd6df5195fc6bd3c1bf0f82fff193bf93
Aziz-dev22 <testnetmelli@gmail.com>
2026-07-13 09:53:44 +0330

    Refactor get_session function in session.py

--- database/session.py
+++ database/session.py
@@ -5,33 +5,16 @@ Database Session Manager
 
 from __future__ import annotations
 
-
 from typing import AsyncGenerator
-
-
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from database.connection import AsyncSessionFactory
 
 
-from database.connection import (
-    AsyncSessionFactory,
-)
-
-
-
-async def get_session()
--> AsyncGenerator[AsyncSession, None]:
-
-
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionFactory() as session:
-
-
         try:
-
             yield session
-
-
         finally:
-
             await session.close()
