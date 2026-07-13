3a4ff50380ce432c86766270ab023884816693cd
Aziz-dev22 <testnetmelli@gmail.com>
2026-07-13 10:38:02 +0330

    Refactor database connection setup and imports

--- database/connection.py
+++ database/connection.py
@@ -5,67 +5,40 @@ Database Connection
 
 from __future__ import annotations
 
-
 from sqlalchemy.ext.asyncio import (
-
-    create_async_engine,
-
     AsyncEngine,
-
+    AsyncSession,
+    create_async_engine,
 )
-
-
-from sqlalchemy.orm import (
-
-    sessionmaker,
-
-)
-
+from sqlalchemy.orm import sessionmaker
 
 from core.config import settings
 
 
-
 DATABASE_URL = (
-
     f"postgresql+asyncpg://"
-
     f"{settings.DATABASE_USER}:"
-
     f"{settings.DATABASE_PASSWORD}@"
-
     f"{settings.DATABASE_HOST}:"
-
     f"{settings.DATABASE_PORT}/"
-
     f"{settings.DATABASE_NAME}"
-
 )
-
 
 
 engine: AsyncEngine = create_async_engine(
-
     DATABASE_URL,
-
     echo=settings.APP_DEBUG,
-
+    future=True,
     pool_pre_ping=True,
-
 )
-
 
 
 AsyncSessionFactory = sessionmaker(
-
     bind=engine,
-
+    class_=AsyncSession,
     expire_on_commit=False,
-
 )
 
 
-
-async def close_database():
-
+async def close_database() -> None:
     await engine.dispose()
